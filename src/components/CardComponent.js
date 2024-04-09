82f29a787dedeff6381c430daf2a4da163e99e0b
Sokheng Tey <sokheng.tey03@gmail.com>
2024-04-09 22:12:57 +0700

    slightly adjustment

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -40,7 +40,7 @@ const CardComponent = ({ movie }) => {
     <main>
       <Link href={`/detail/${movie_id}`}>
         <div className="bg-white w-[17rem] px-3 py-3 mt-1 rounded-md">
-          <img className="rounded-sm" src={imageUrl} alt={movie_title} />
+          <img className="rounded-sm w-[17rem] h-[8.7rem]" src={imageUrl} alt={movie_title} />
           <TruncatedMovieTitle className="mt-3 text-lg font-bold tracking-tight text-gray-900 dark:text-white">
             {movie_title}
           </TruncatedMovieTitle>
